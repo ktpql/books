c3e1aa97470316615b47b4be9d581ecf6db59c5f
Mustafa Katipoğlu <mustafa.katipoglu@outlook.com>
2024-01-01 11:09:46 +0300

    fix: update tests according to new homepage

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,10 +1,10 @@
 import { browser, expect } from '@wdio/globals'
 
-describe('Title should include Hello World', () => {
-    it('should print application title', async () => {
+describe('Title should include Ktpql Books', () => {
+    it('should show application title', async () => {
         const pageTitle = await browser.getTitle();
 
-        expect(pageTitle).toContain('Hello World');
+        expect(pageTitle).toContain('Ktpql Books');
     })
 })
 
