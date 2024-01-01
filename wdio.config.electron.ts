01378f0da907ab293b85fb02bc1cfcb146d7fc30
Mustafa Katipoğlu <mustafa.katipoglu@outlook.com>
2024-01-01 15:27:25 +0300

    build: integrate react unit testing

--- wdio.config.electron.ts
+++ wdio.config.electron.ts
@@ -31,7 +31,7 @@ export const config: Options.Testrunner = {
     // of the config file unless it's absolute.
     //
     specs: [
-        './test/specs/**/*.ts'
+        './test/integration/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [
